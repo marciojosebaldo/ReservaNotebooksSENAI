118bf2752bfaf4c4931b75f3e40a5318b40cc261
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-20 19:57:28 -0300

    Add files via upload

---
+++ src/main/java/ReservaNotebooks/ReservaNotebooksSENAI/ReservaNotebooksSenaiApplication.java
@@ -0,0 +1,13 @@
+package ReservaNotebooks.ReservaNotebooksSENAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaNotebooksSenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaNotebooksSenaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ReservaNotebooks/ReservaNotebooksSENAI/ReservaNotebooksSenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package ReservaNotebooks.ReservaNotebooksSENAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaNotebooksSenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
