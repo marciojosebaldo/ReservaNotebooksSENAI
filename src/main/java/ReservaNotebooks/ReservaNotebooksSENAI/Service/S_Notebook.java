f81f8e52f617396a9d17b1b9df38f2c0fdf2ee9d
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-22 20:25:42 -0300

    HTML

---
+++ src/main/java/ReservaNotebooks/ReservaNotebooksSENAI/Service/S_Notebook.java
@@ -0,0 +1,17 @@
+package ReservaNotebooks.ReservaNotebooksSENAI.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class S_Notebook {
+
+    public String cadastrarNotebook(String numero, String patrimonio) {
+        boolean podeSalvar;
+        String mensagem = "";
+
+
+
+
+        return null;
+    }
+}
