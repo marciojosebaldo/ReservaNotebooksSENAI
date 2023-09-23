c162905abbb94d23dafdc2f1c8e88c19190bd304
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-22 22:24:17 -0300

    R

--- src/main/java/ReservaNotebooks/ReservaNotebooksSENAI/Repository/R_Notebook.java
+++ src/main/java/ReservaNotebooks/ReservaNotebooksSENAI/Repository/R_Notebook.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 
 // O repository entende que é um save, ou seja, é um padrão porque está de acordo com o Model.
 // Todo o CRUD quando é tabela completa, dispensa o query
+// A query só é escrita quando a consulta é personalizada, ou seja, quando utilizam join e outros elementos
+// para busca em um banco de dados
 
 @Repository
 public interface R_Notebook extends JpaRepository<M_Notebook, Long> {
