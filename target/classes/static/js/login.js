4dfab10326fc6529a4366965221ecb1cfbcc8bfb
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-27 19:17:52 -0300

    html

---
+++ target/classes/static/js/login.js
@@ -0,0 +1,21 @@
+$("#btnLogin").click(fazerLogin);
+
+    function fazerLogin() {
+        let matricula = $("#matricula").val();
+        let senha = $("#senha").val();
+
+        $.ajax({
+            type: "POST",
+            url: "/login",
+            data: {
+                matricula: matricula,
+                senha: senha
+            },
+            success: function(data){
+                alert(data);
+            },
+            error: function() {
+                alert("Falha na comunicação com o servidor!");
+            }
+        });
+    }
