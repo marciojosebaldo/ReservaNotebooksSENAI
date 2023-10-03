de99fb9f6244668bf001e186f84a096e6c6d4a8e
marciojosebaldo <marciojosebaldo@gmail.com>
2023-10-02 22:16:47 -0300

    routes.js

---
+++ target/classes/static/js/routes.js
@@ -0,0 +1,13 @@
+//$.get("/cadastro/notebook", function(event){
+//    $(this).preventDefault();
+//    alert("Teste");
+//});
+
+$("#cadastroNotebook").click(function(event) {
+    event.preventDefault(); // Isso previne a ação padrão do link
+
+    $.get("/cadastro/notebook", function(data) {
+        // O código aqui será executado após a conclusão da solicitação AJAX
+        alert("Teste");
+    });
+});
