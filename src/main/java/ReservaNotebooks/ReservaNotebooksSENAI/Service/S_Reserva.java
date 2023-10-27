2c7ec88112087be822a6b74024b61e6de90c01a8
marciojosebaldo <marciojosebaldo@gmail.com>
2023-10-27 20:37:12 -0300

    reserva

--- src/main/java/ReservaNotebooks/ReservaNotebooksSENAI/Service/S_Reserva.java
+++ src/main/java/ReservaNotebooks/ReservaNotebooksSENAI/Service/S_Reserva.java
@@ -1,5 +1,6 @@
 package ReservaNotebooks.ReservaNotebooksSENAI.Service;
 
+import ReservaNotebooks.ReservaNotebooksSENAI.Model.M_Reserva;
 import ReservaNotebooks.ReservaNotebooksSENAI.Repository.R_Reserva;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
@@ -15,11 +16,17 @@ public class S_Reserva {
 
     public static String cadastrarReservaNotebook(Long idUsuario, String quantidade, String data_ini,
                                                   String data_fin){
-
-
-
         boolean podeSalvar = true;
         String mensagem = "";
+
+        M_Reserva m_reserva = new M_Reserva();
+
+        m_reserva.setId_usuario(idUsuario);
+        m_reserva.setQuantidade(Long.parseLong(quantidade));
+        m_reserva.setData_ini(data_ini);
+        m_reserva.setData_fim(data_fin);
+
+
 
         return "";
     }
