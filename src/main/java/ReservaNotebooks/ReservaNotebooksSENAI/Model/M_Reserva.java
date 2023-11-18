1b5bf267338b50711f7a1521bc172c30e6288299
marciojosebaldo <marciojosebaldo@gmail.com>
2023-11-17 21:02:13 -0300

    limpeza código

--- src/main/java/ReservaNotebooks/ReservaNotebooksSENAI/Model/M_Reserva.java
+++ src/main/java/ReservaNotebooks/ReservaNotebooksSENAI/Model/M_Reserva.java
@@ -3,7 +3,6 @@ package ReservaNotebooks.ReservaNotebooksSENAI.Model;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Entity
--- src/main/java/ReservaNotebooks/ReservaNotebooksSENAI/Service/S_Generico.java
+++ src/main/java/ReservaNotebooks/ReservaNotebooksSENAI/Service/S_Generico.java
@@ -1,15 +1,9 @@
 package ReservaNotebooks.ReservaNotebooksSENAI.Service;
 
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Test;
 
 @Service
 public class S_Generico {
@@ -24,16 +18,6 @@ public class S_Generico {
     public static LocalDate dataAtualRegistro(){
         return LocalDate.now();
     }
-
-//    // Teste para conferência da data em String
-//    @Test
-//    public void TesteDataAtualRegistro(){
-//        LocalDateTime dataAtual = LocalDateTime.now();
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-//        String dataAtualEsperada = dataAtual.format(formatter);
-//        String dataAtualRegistro = S_Generico.dataAtualRegistro();
-//        assertEquals(dataAtualEsperada, dataAtualRegistro);
-//    }
 
     public static LocalDate StringParaDate(String data){
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
