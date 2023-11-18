723a4c4286d1348dd6f15ecec0d78a779b495c6f
marciojosebaldo <marciojosebaldo@gmail.com>
2023-11-17 21:57:18 -0300

    home

--- src/main/java/ReservaNotebooks/ReservaNotebooksSENAI/Controller/C_Home.java
+++ src/main/java/ReservaNotebooks/ReservaNotebooksSENAI/Controller/C_Home.java
@@ -1,10 +1,13 @@
 package ReservaNotebooks.ReservaNotebooksSENAI.Controller;
 
+import ReservaNotebooks.ReservaNotebooksSENAI.Model.M_Reserva;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class C_Home {
@@ -12,6 +15,11 @@ public class C_Home {
     public String getHome(HttpSession session,
                           Model model) {
         if (session.getAttribute("usuario") != null) {
+            // Duas últimas linhas estão abaixo do tbody da view do home.html. Servem para invocar os dados salvos
+            // Precisa salvar os dados
+            List<M_Reserva> listaDeReservas = ;
+            model.addAttribute("listaDeReservas", listaDeReservas);
+
             model.addAttribute("usuario", session.getAttribute("usuario"));
             return "Home/home";
         } else {
